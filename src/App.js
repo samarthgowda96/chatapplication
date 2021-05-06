e9e7849b42472e81d9a073eab43aaca333ee59ac
Samarth Gowda <samarthgowda@Samarths-MBP.fios-router.home>
2021-05-06 00:13:05 -0400

    created components, testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import  {ChatEngine} from 'react-chat-engine'
+import './App.css'
+import LoginForm from './LoginForm'
+import ChatFeed from './ChatFeed'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+    if(!localStorage.getItem('username')) return <LoginForm/>
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="621e31b7-9f92-4697-9b51-afdedc6a816a"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+             
+
+        />
+    )
 }
 
 export default App;
